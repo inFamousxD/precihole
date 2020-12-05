023593efb9c87f5abef2cdcf210af8f2f874aba1
Aaditya Mankar <infamous@pop-os.localdomain>
2020-12-05 17:23:41 +0530

    1

--- src/components/Navigation/NavInteraction.js
+++ src/components/Navigation/NavInteraction.js
@@ -8,7 +8,14 @@ import {
 const NavInteraction = () => {
     return (
         <Row>
-
+            <Col xs={6} lg={3} md={3} xl={3}>
+                <Button variant='secondary-outline' style={{
+                    color: '#aaa',
+                    paddingTop: '15px'
+                }}>
+                    Home
+                </Button>
+            </Col>
             <Col xs={6} lg={3} md={3} xl={3}>
                 <Button variant='secondary-outline' style={{
                     color: '#aaa',
